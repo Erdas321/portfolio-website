9d75019b4bb931de9bb871f9b3f499c194510e59
Erdas321 <103915466+Erdas321@users.noreply.github.com>
2025-04-08 08:59:46 +0300

    Added fade in animation

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 function login() {
     document.getElementById("laptop_display").style.display = "grid"; // Show projects
+    document.getElementById("laptop_display").style.animation = "fadeIn 2s forwards";
     /*document.getElementById("taskbar").style.display = "block"; // Hide image section*/
     document.getElementById("login_screen").style.display = "none";
 }
@@ -7,5 +8,6 @@ function login() {
 function logoff() {
     document.getElementById("laptop_display").style.display = "none"; // Show projects
     /*document.getElementById("taskbar").style.display = "none"; // Hide image section*/
+    document.getElementById("login_screen").style.animation = "fadeOut 2s forwards";
     document.getElementById("login_screen").style.display = "block";
 }
