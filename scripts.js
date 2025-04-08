4324668c9c3f3e4d455736f2efa3277b7ea1d706
Erdas321 <103915466+Erdas321@users.noreply.github.com>
2025-04-08 09:12:41 +0300

    Delayed log in message

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 function login() {
-    document.getElementById("laptop_display").style.display = "grid"; // Show projects
-    document.getElementById("laptop_display").style.animation = "fadeIn 2s forwards";
-    /*document.getElementById("taskbar").style.display = "block"; // Hide image section*/
+    document.getElementById("laptop_display").style.display = "grid";
     document.getElementById("login_screen").style.display = "none";
+    document.getElementById("laptop_display").style.animation = "fadeIn 2s forwards";
 }
 
 function logoff() {
-    document.getElementById("laptop_display").style.display = "none"; // Show projects
-    /*document.getElementById("taskbar").style.display = "none"; // Hide image section*/
-    document.getElementById("login_screen").style.animation = "fadeOut 2s forwards";
+    document.getElementById("laptop_display").style.display = "none";
     document.getElementById("login_screen").style.display = "block";
+    document.getElementById("login_screen").style.animation = "fadeIn 2s forwards";
+    document.getElementById("img_text").style.animation = "fadeOut 15s forwards";
 }
